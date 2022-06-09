74b11526de90d6604cfbfdc241ed27e37bd335c0
KramerEd <danat7531@gmail.com>
2022-06-09 13:08:19 +0300

    add modal]

--- js/script.js
+++ js/script.js
@@ -5,16 +5,39 @@ const burgerBody = document.querySelector(".header__burger");
 const blur = document.querySelector(".blur");
 const linksContainer = document.querySelector(".header__links");
 
-const toggleElement = () => {
+burgerWrapper.addEventListener("click", () => {
 	burgerBody.classList.toggle("active");
 	linksContainer.classList.toggle("active");
 	body.classList.toggle("active");
 	blur.classList.toggle("active");
-};
-
-burgerWrapper.addEventListener("click", () => {
-	toggleElement();
 });
 blur.addEventListener("click", () => {
-	if (blur.classList.contains("active")) toggleElement();
+	if (blur.classList.contains("active")) {
+		burgerBody.classList.toggle("active");
+		linksContainer.classList.toggle("active");
+		body.classList.toggle("active");
+		blur.classList.toggle("active");
+	}
+});
+
+const modal = document.querySelector(".modal");
+const form = document.querySelector(".fifth__form");
+const closeButt = document.querySelector("#modal-close");
+const formBlur = document.querySelector(".blur-form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	modal.classList.toggle("active");
+	body.classList.toggle("active");
+	formBlur.classList.toggle("active");
+});
+closeButt.addEventListener("click", () => {
+	modal.classList.toggle("active");
+	body.classList.toggle("active");
+	formBlur.classList.toggle("active");
+});
+formBlur.addEventListener("click", () => {
+	formBlur.classList.toggle("active");
+	modal.classList.toggle("active");
+	body.classList.toggle("active");
 });
