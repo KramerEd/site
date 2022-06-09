f30dda35a2dfb42907ec2c29cfef2698638649b3
KramerEd <danat7531@gmail.com>
2022-06-09 12:08:03 +0300

    add burger

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
+const body = document.body;
+
 const burgerWrapper = document.querySelector(".burger");
 const burgerBody = document.querySelector(".header__burger");
-burgerWrapper.addEventListener("click", () => {
+const blur = document.querySelector(".blur");
+const linksContainer = document.querySelector(".header__links");
+
+const toggleElement = () => {
 	burgerBody.classList.toggle("active");
+	linksContainer.classList.toggle("active");
+	body.classList.toggle("active");
+	blur.classList.toggle("active");
+};
+
+burgerWrapper.addEventListener("click", () => {
+	toggleElement();
+});
+blur.addEventListener("click", () => {
+	if (blur.classList.contains("active")) toggleElement();
 });
